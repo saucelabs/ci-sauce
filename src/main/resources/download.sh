929dafd4b44774af1da18d61a3f4d29ab232c202
Gavin Mogan <gavin@saucelabs.com>
2016-03-22 18:47:29 -0700

    update download script to update java file too

--- src/main/resources/download.sh
+++ src/main/resources/download.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+URL=${URL:=https://saucelabs.com/versions.json}
+
 rm sc-*.tar.gz sc-*.zip
-curl https://saucelabs.com/versions.json | jq '.["Sauce Connect"] | values[] | select (type=="object") | .download_url' | xargs wget
+curl -s $URL | jq '.["Sauce Connect"] | values[] | select (type=="object") | .download_url' | xargs wget
+CURRENT_SC_VERSION=$(curl -s $URL | jq '.["Sauce Connect"].version')
+perl -pi -e "s/String CURRENT_SC_VERSION = \"(\d+\.?)+\"/String CURRENT_SC_VERSION = $CURRENT_SC_VERSION/" $DIR/../java/com/saucelabs/ci/sauceconnect/SauceConnectFourManager.java
