2654373d0965f087c13d8dff7b7b00cb4f2e7af2
Gavin Mogan <gavin@saucelabs.com>
2016-07-13 10:32:33 -0700

    getLogfile and getTunnelIdentifier should return the last one provided, add tests as such

---
+++ src/test/java/com/saucelabs/ci/sauceconnect/AbstractSauceTunnelManagerTest.java
@@ -0,0 +1,21 @@
+package com.saucelabs.ci.sauceconnect;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gavinmogan on 2016-07-13.
+ */
+public class AbstractSauceTunnelManagerTest {
+
+    @Test
+    public void testGetTunnelIdentifier() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLogfile() throws Exception {
+
+    }
+}
