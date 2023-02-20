3e862fe2267fbc9f631895b8b7385815595a7fcc
Valery Yatsynovich <valery_yatsynovich@epam.com>
2023-02-20 15:54:33 +0300

    Use enum to specify desired data center

--- src/main/java/com/saucelabs/ci/sauceconnect/SauceTunnelManager.java
+++ src/main/java/com/saucelabs/ci/sauceconnect/SauceTunnelManager.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 
+import com.saucelabs.saucerest.DataCenter;
+
 /**
  * Interface which defines the behaviour for Sauce Connect Tunnel implementations.
  *
@@ -42,7 +44,26 @@ public interface SauceTunnelManager {
      *
      * @param username         the name of the Sauce OnDemand user
      * @param apiKey           the API Key for the Sauce OnDemand user
-     * @param dataCenter       the Sauce Labs Data Center name (US, EU, US_EAST)
+     * @param dataCenter       the Sauce Labs Data Center name (US_WEST, EU_CENTRAL, US_EAST, APAC_SOUTHEAST)
+     * @param port             the port which Sauce Connect should be run on
+     * @param sauceConnectJar  the Jar file containing Sauce Connect.  If null, then we attempt to find Sauce Connect from the classpath (only used by SauceConnectTwoManager)
+     * @param options          the command line options to pass to Sauce Connect
+     * @param printStream      A print stream in which to redirect the output from Sauce Connect to.  Can be null
+     * @param verboseLogging   indicates whether verbose logging should be output
+     * @param sauceConnectPath if defined, Sauce Connect will be launched from the specified path and won't be extracted from the jar file
+     * @return a {@link Process} instance which represents the Sauce Connect instance
+     * @throws IOException thrown if an error occurs launching Sauce Connect
+     * @deprecated Use {@link #openConnection(String, String, DataCenter, int, File, String, PrintStream, Boolean, String)} instead
+     */
+    @Deprecated
+    Process openConnection(String username, String apiKey, String dataCenter, int port, File sauceConnectJar, String options, PrintStream printStream, Boolean verboseLogging, String sauceConnectPath) throws IOException;
+
+    /**
+     * Creates a new process to run Sauce Connect.
+     *
+     * @param username         the name of the Sauce OnDemand user
+     * @param apiKey           the API Key for the Sauce OnDemand user
+     * @param dataCenter       the Sauce Labs Data Center
      * @param port             the port which Sauce Connect should be run on
      * @param sauceConnectJar  the Jar file containing Sauce Connect.  If null, then we attempt to find Sauce Connect from the classpath (only used by SauceConnectTwoManager)
      * @param options          the command line options to pass to Sauce Connect
@@ -52,6 +73,6 @@ public interface SauceTunnelManager {
      * @return a {@link Process} instance which represents the Sauce Connect instance
      * @throws IOException thrown if an error occurs launching Sauce Connect
      */
-    Process openConnection(String username, String apiKey, String dataCenter, int port, File sauceConnectJar, String options, PrintStream printStream, Boolean verboseLogging, String sauceConnectPath) throws IOException;
+    Process openConnection(String username, String apiKey, DataCenter dataCenter, int port, File sauceConnectJar, String options, PrintStream printStream, Boolean verboseLogging, String sauceConnectPath) throws IOException;
 
 }
