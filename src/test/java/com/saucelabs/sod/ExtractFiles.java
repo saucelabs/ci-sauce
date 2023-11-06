64dc985f8b1fc7919c1dd9ae0615f5958b0bfb47
Valery Yatsynovich <valery_yatsynovich@epam.com>
2023-11-06 23:14:03 +0200

    Replace `plexus-archiver` with Apache `commons-compress`

--- src/test/java/com/saucelabs/sod/ExtractFiles.java
+++ src/test/java/com/saucelabs/sod/ExtractFiles.java
@@ -1,27 +1,42 @@
 package com.saucelabs.sod;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 import com.saucelabs.ci.sauceconnect.SauceConnectFourManager;
-import org.junit.jupiter.api.Test;
+import com.saucelabs.ci.sauceconnect.SauceConnectFourManager.OperatingSystem;
+import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 /**
  * @author Ross Rowe
  */
 class ExtractFiles {
 
-  private SauceConnectFourManager manager = new SauceConnectFourManager();
+  private final SauceConnectFourManager manager = new SauceConnectFourManager();
 
-  @Test
-  void linux() throws Exception {
-
-    File workingDirectory = new File(System.getProperty("java.io.tmpdir"));
-    manager.extractZipFile(workingDirectory, SauceConnectFourManager.OperatingSystem.LINUX);
+  static Stream<Arguments> operatingSystems() {
+    return Stream.of(
+      arguments(OperatingSystem.OSX, "sc"),
+      arguments(OperatingSystem.LINUX, "sc"),
+      arguments(OperatingSystem.LINUX_ARM64, "sc"),
+      arguments(OperatingSystem.WINDOWS, "sc.exe")
+    );
   }
 
-  @Test
-  void windows() throws Exception {
-    File workingDirectory = new File(System.getProperty("java.io.tmpdir"));
-    manager.extractZipFile(workingDirectory, SauceConnectFourManager.OperatingSystem.WINDOWS);
+  @ParameterizedTest
+  @MethodSource("operatingSystems")
+  void shouldExtractSauceConnectExecutable(OperatingSystem os, String executableFileName, @TempDir Path tempDir)
+      throws IOException {
+    File dir = manager.extractZipFile(tempDir.toFile(), os);
+    File executableFile = dir.toPath().resolve("bin/" + executableFileName).toFile();
+    assertTrue(executableFile.exists());
   }
 }
